bf005dfbefeb75becec703894a8f671e19dce14e
Boomlnwza007 <Rangsimon2014@gmail.com>
2024-08-26 23:23:00 +0700

    Update SpriteFlash.cs

--- Operation_Escape/Assets/Code/System/SpriteFlash.cs
+++ Operation_Escape/Assets/Code/System/SpriteFlash.cs
@@ -6,6 +6,7 @@ public class SpriteFlash : MonoBehaviour
 {
     public Material flashMaterial;
     private Material originalMaterial;
+    private Color originalColor;
     private SpriteRenderer spriteRenderer;
 
     public float duration = 0.1f;
@@ -15,6 +16,7 @@ public class SpriteFlash : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         originalMaterial = spriteRenderer.material;
+        originalColor = spriteRenderer.color;
     }
 
     public void Flash()
@@ -24,10 +26,12 @@ public class SpriteFlash : MonoBehaviour
 
     private IEnumerator FlashRun()
     {
+        spriteRenderer.color = Color.white;
         spriteRenderer.material = flashMaterial;
 
         yield return new WaitForSeconds(duration);
 
+        spriteRenderer.color = originalColor;
         spriteRenderer.material = originalMaterial;
     }
 }
