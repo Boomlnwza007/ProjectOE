43af9c056a356ae4aa7ae4142b3e2bda983ee76d
Boomlnwza007 <Rangsimon2014@gmail.com>
2024-09-17 23:56:12 +0700

    add Door Slide

--- Operation_Escape/Assets/Code/Door/SlideDoor.cs
+++ Operation_Escape/Assets/Code/Door/SlideDoor.cs
@@ -4,23 +4,64 @@ using UnityEngine;
 
 public class SlideDoor : TypeDoor
 {
-    public GameObject doorL;
-    public GameObject doorR;
-    private float sizeDoor;
+    public Transform doorL;
+    public Transform doorR;
+    public float speed;
+    public float sizeDoor;
+    public bool isLR;
+    private Vector3 doorFPosition;
+    private Vector3 doorSPosition;
+    private Vector3 targetDoorFPosition;
+    private Vector3 targetDoorSPosition;
+    private bool isOpen;
+    private bool isClose;
 
-    private void Start()
+    private void Awake()
     {
-        sizeDoor = doorL.GetComponentInChildren<Transform>().localScale.x;
+        doorFPosition = doorL.position;
+        doorSPosition = doorR.position;
+
+        targetDoorFPosition = doorFPosition;
+        targetDoorSPosition = doorSPosition;
+
+        if (isLR)
+        {
+            targetDoorFPosition.x -= sizeDoor;
+            targetDoorSPosition.x += sizeDoor;
+        }
+        else
+        {
+            targetDoorFPosition.y += sizeDoor;
+            targetDoorSPosition.y -= sizeDoor;
+        }
+        
     }
 
     protected override void Openning()
     {
-
+        isOpen = true;
+        isClose = false;
     }
 
     protected override void Closesing()
     {
+        isClose = true;
+        isOpen = false;
+    }
 
+    private void FixedUpdate()
+    {
+        if (isOpen)
+        {
+            doorL.position = Vector2.MoveTowards(doorL.position, targetDoorFPosition, speed * Time.deltaTime);
+            doorR.position = Vector2.MoveTowards(doorR.position, targetDoorSPosition, speed * Time.deltaTime);
+        }
+
+        if (isClose)
+        {
+            doorL.position = Vector2.MoveTowards(doorL.position, doorFPosition, speed * Time.deltaTime);
+            doorR.position = Vector2.MoveTowards(doorR.position, doorSPosition, speed * Time.deltaTime);
+        }
     }
 
 }
