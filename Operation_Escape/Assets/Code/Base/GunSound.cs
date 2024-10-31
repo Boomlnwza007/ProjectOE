5a4bf1a16ae9adaf21982bc174b3223ca28bcc9d
Boomlnwza007 <Rangsimon2014@gmail.com>
2024-10-31 16:21:20 +0700

    Audio Manager

---
+++ Operation_Escape/Assets/Code/Base/GunSound.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "GunName")]
+public class GunSound : ScriptableObject
+{
+    public AudioClip shoot;
+    public AudioClip shootUltimate;
+}
--- Operation_Escape/Assets/Code/Player/PlayerSound.cs
+++ Operation_Escape/Assets/Code/Player/PlayerSound.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class PlayerSound : MonoBehaviour
 {
+    public static PlayerSound playerSound;
 
+    [Header("----- Audio Clip -----")]
+    public AudioClip ChangGun;
+    public AudioClip gunFail;
+    public AudioClip ultimateOn;
+    public AudioClip ultimateOff;
+    public AudioClip reload;
+    public AudioClip melee;
+    public AudioClip dash;
+    public AudioClip walk;
+
+
+    private void Awake()
+    {
+        playerSound = this;
+    }
+
+    public void Playwalk()
+    {
+        AudioManager.audioManager.PlaySFX(walk);
+    }
+
+    public void Playdash()
+    {
+        AudioManager.audioManager.PlaySFX(dash);
+    }
+
+    public void PlayMelee()
+    {
+        AudioManager.audioManager.PlaySFX(melee);
+    }
 }
