2e7caab1a74ad9c4970d66eaac53937607a3d0ed
Boomlnwza007 <Rangsimon2014@gmail.com>
2024-11-09 00:36:13 +0700

    Blade Bullet

---
+++ Operation_Escape/Assets/Code/Bullet/ShockwaveBullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockwaveBullet : BaseBullet
+{
+    bool hit;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ready = true;
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == tagUse)
+        {
+            if (!hit)
+            {
+                hit = true;
+                IDamageable target = collision.GetComponent<IDamageable>();
+                if (target != null)
+                {
+                    target.Takedamage(damage, DamageType.Rang, knockbackForce);
+                    KnockBackPush(collision);
+                }
+            }            
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
