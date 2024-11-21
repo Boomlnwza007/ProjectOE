6403873a888dbcc9136b31d23a1ad4dc327b0fdc
641310523 <pichetpong31@gmail.com>
2024-11-21 23:26:44 +0700

    cutscene game start

--- Operation_Escape/Assets/Code/System/Parallax.cs
+++ Operation_Escape/Assets/Code/System/Parallax.cs
@@ -16,10 +16,14 @@ public class Parallax : MonoBehaviour
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
         transform.position = Vector3.Lerp(transform.position, new Vector3(startpos + dist, transform.position.y, transform.position.z), smooth);
         //transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
