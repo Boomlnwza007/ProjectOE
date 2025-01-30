2388ecee5186236652614db1f05c258386ee494e
Boomlnwza007 <Rangsimon2014@gmail.com>
2025-01-30 22:30:01 +0700

    E sound

--- Operation_Escape/Assets/Code/Base/AudioCharacter.cs
+++ Operation_Escape/Assets/Code/Base/AudioCharacter.cs
@@ -6,12 +6,12 @@ public class AudioCharacter : MonoBehaviour
 {
     public AudioSource audioGame;
     [Header("------ Audio Base ------")]
-    public AudioClip walk;
+    public AudioClip[] walk;
     public AudioClip[] attack;
 
-    public void PlayWalk()
+    public void PlayWalk(int index)
     {
-        audioGame.PlayOneShot(walk);
+        audioGame.PlayOneShot(walk[index]);
     }
 
     public void PlayAtttack(int index)
