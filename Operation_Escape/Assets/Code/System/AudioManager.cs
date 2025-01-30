2d20afa205bdbd364de5771d08504701ecb003c4
Boomlnwza007 <Rangsimon2014@gmail.com>
2025-01-30 23:45:11 +0700

    Change Bgm np setUp

--- Operation_Escape/Assets/Code/System/AudioManager.cs
+++ Operation_Escape/Assets/Code/System/AudioManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager audioManager;
+    public float fadeDuration = 2f;
     [Header("----- Audio Soure -----")]
-    public AudioSource musicSource;
+    public AudioSource musicBGMSource;
+    public AudioSource newMusicBGMSource;
     public AudioSource sfxSource;
 
     [Header("----- Audio Clip -----")]
     public AudioClip backGround;
+    //public AudioClip[] backGroundBoss;
     public AudioClip gameOver;
     public AudioClip click;
 
@@ -21,12 +25,43 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        musicSource.clip = backGround;
-        musicSource.Play();
+        musicBGMSource.clip = backGround;
+        musicBGMSource.Play();
     }
 
     public void PlaySFX(AudioClip clip)
     {
         sfxSource.PlayOneShot(clip);
+    }
+
+    public async UniTask ChangeBGM(AudioClip newClip)
+    {
+        if (musicBGMSource.clip == newClip) return;
+
+        newMusicBGMSource.clip = newClip;
+        newMusicBGMSource.volume = 0;
+        newMusicBGMSource.Play();
+
+        await CrossfadeBGM();
+
+        musicBGMSource.Stop();
+        (musicBGMSource, newMusicBGMSource) = (newMusicBGMSource, musicBGMSource);
+    }
+
+    private async UniTask CrossfadeBGM()
+    {
+        float timer = 0f;
+        float startVolume = musicBGMSource.volume;
+
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            float progress = timer / fadeDuration;
+
+            musicBGMSource.volume = Mathf.Lerp(startVolume, 0, progress);
+            newMusicBGMSource.volume = Mathf.Lerp(0, startVolume, progress);
+
+            await UniTask.Yield(PlayerLoopTiming.Update);
+        }
     }
 }
