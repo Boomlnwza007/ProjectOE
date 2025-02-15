99e6f6cccdc12e0d6176f2570ccd00377d283927
Boomlnwza007 <Rangsimon2014@gmail.com>
2025-02-15 19:50:12 +0700

    Bullet M2

--- Operation_Escape/Assets/Code/Bullet/BulletMinion.cs
+++ Operation_Escape/Assets/Code/Bullet/BulletMinion.cs
@@ -9,43 +9,50 @@ public class BulletMinion : BaseBullet
     private float yTarget;
     private float yPos;
     private bool yPosSet = false;
+    public float distance = 10;
+    public float offset = 10;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        yPos = transform.position.y;
         rb.velocity = transform.up * speed;
     }
 
     private void Update()
     {
-        if (!sprite.isVisible && !shoot)
+        if (Mathf.Abs(transform.position.y - yPos) > distance && !yPosSet)
         {
-            if (!yPosSet)
+            Debug.Log(transform.position.y + " " + yPos);
+            yPos = transform.position.y;
+            yPosSet = true;
+        }
+        else if(yPosSet)
+        {
+            if (!sprite.isVisible && !shoot)
             {
-                yPos = transform.position.y;
-                yPosSet = true;
+                if (transform.position.y > yPos + offset)
+                {
+                    ready = true;
+                    yTarget = CinemachineControl.Instance.player.position.y;
+                    transform.position = new Vector3(CinemachineControl.Instance.player.position.x, transform.position.y, transform.position.z);
+                    rb.velocity = -transform.up * speed;
+                    shoot = true;
+                   // Debug.Log(transform.position.y + " "+yPos + 10);
+                }
+                return;
             }
-
-            if (transform.position.y  > yPos + 5)
+            else if (transform.position.y <= yTarget && shoot)
             {
-                ready = true;
-                yTarget = CinemachineControl.Instance.player.position.y;
-                transform.position = new Vector3(CinemachineControl.Instance.player.position.x, transform.position.y, transform.position.z);
-                rb.velocity = -transform.up * speed;
-                shoot = true;
-            }            
-            return;
-        }
-        else if (transform.position.y <= yTarget && shoot)
-        {
-            Destroy(gameObject);
-        }
+                Destroy(gameObject);
+            }
+        }        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == tagUse)
+        if (collision.tag == tagUse && ready)
         {
             IDamageable target = collision.GetComponent<IDamageable>();
             if (target != null)
