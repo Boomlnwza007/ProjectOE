714e5c147dbbf1dadccf7513fd04dc3c2402ab6b
Boomlnwza007 <Rangsimon2014@gmail.com>
2025-03-06 18:27:50 +0700

    Boss2 add Phash2

--- Operation_Escape/Assets/Code/Enemy/Boss2_FSM_Enemy/Boss/State/CheckNextB2FSM.cs
+++ Operation_Escape/Assets/Code/Enemy/Boss2_FSM_Enemy/Boss/State/CheckNextB2FSM.cs
@@ -7,35 +7,52 @@ using UnityEngine;
 public class CheckNextB2FSM : BaseState
 {
     public CheckNextB2FSM(FSMBoss2EnemySM stateMachine) : base("Swarm", stateMachine) { }
-    public List<int> rNumber = new List<int> { 1, 2, 3, 4 };
+    public List<int> rNumber = new List<int> { 1, 2, 3};
+    public List<int> rNumberP2 = new List<int> { 1, 2, 3, 4 };
+
     // Start is called before the first frame update
     public override void Enter()
     {
         var state = (FSMBoss2EnemySM)stateMachine;
         if (state.phase)
         {
-            if (rNumber.Count <= 0)
+            if (rNumberP2.Count == 0)
             {
-                rNumber = new List<int> { 1, 2, 3};
+                rNumberP2 = new List<int> { 1, 2, 3};
             }
-            state.merge.stateAtk.Clear();
-            int index = Random.Range(0, rNumber.Count);
-            int selectedAttack = rNumber[index];
-            rNumber.RemoveAt(index);
-            state.merge.stateAtk.Add(selectedAttack);
-            ChangState(state.merge);
+            int index = Random.Range(0, rNumberP2.Count);
+            int selectedAttack = rNumberP2[index];
+            rNumberP2.RemoveAt(index);
+            ChangState(CaseStateP2(selectedAttack));
+            state.curStateName = state.curState.nameState;
         }
         else
         {
             if (rNumber.Count == 0)
             {
-                rNumber = new List<int> { 1, 2, 3, 4 };
+                rNumber = new List<int> { 1, 2, 3};
             }
             int index = Random.Range(0, rNumber.Count);
             int selectedAttack = rNumber[index];
             rNumber.RemoveAt(index);
             ChangState(CaseState(selectedAttack));
             state.curStateName = state.curState.nameState;
+        }
+    }
+
+    public BaseState CaseStateP2(int number)
+    {
+        var state = (FSMBoss2EnemySM)stateMachine;
+        switch (number)
+        {
+            case 1:
+                return state.mStrike;
+            case 2:
+                return state.mArea;
+            case 3:
+                return state.mSwarm;
+            default:
+                return state.mStrike;
         }
     }
 
