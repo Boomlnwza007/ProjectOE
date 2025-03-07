a023a253d77e326715fb008de9759efdfe423ce1
Boomlnwza007 <Rangsimon2014@gmail.com>
2025-03-08 05:55:12 +0700

    Update CheckInRoom.cs

--- Operation_Escape/Assets/Code/Enemy/Boss2_FSM_Enemy/Base/CheckInRoom.cs
+++ Operation_Escape/Assets/Code/Enemy/Boss2_FSM_Enemy/Base/CheckInRoom.cs
@@ -17,6 +17,19 @@ public class CheckInRoom : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out FSMBoss2EnemySM state))
+        {
+            state.inRoom = true;
+        }
+
+        if (collision.gameObject.TryGetComponent(out DummyBoss2 dummy))
+        {
+            dummy.inRoom = true;
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out FSMBoss2EnemySM state))
